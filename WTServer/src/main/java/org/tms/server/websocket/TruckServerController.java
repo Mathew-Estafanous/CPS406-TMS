1c4d26acd1fd3dbae321d888f173bffe9eb4365f
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-03-13 19:17:47 -0400

    Create TruckMessage object for websocket messages.

--- WTServer/src/main/java/org/tms/server/websocket/TruckServerController.java
+++ WTServer/src/main/java/org/tms/server/websocket/TruckServerController.java
@@ -3,27 +3,32 @@ package org.tms.server.websocket;
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.logging.Logger;
 
-@ServerEndpoint(value = "/truck/server")
+@ServerEndpoint(value = "/truck/server",
+                decoders = TruckMessage.TruckMessageDecoder.class,
+                encoders = TruckMessage.TruckMessageEncoder.class)
 public class TruckServerController {
+
+    private static final Logger log = Logger.getLogger(TruckServerController.class.getName());
 
     @OnOpen
     public void onOpen(Session session) throws IOException {
-        System.out.println("Connection opened");
+        log.info("New connection opened");
     }
 
     @OnMessage
-    public void onMessage(Session session, String message) throws IOException {
-        System.out.println("Message received: " + message);
+    public void onMessage(Session session, TruckMessage message) throws IOException {
+        log.info("Message received: " + message);
     }
 
     @OnClose
     public void onClose(Session session) throws IOException {
-        System.out.println("Connection closed");
+        log.info("Connection closed");
     }
 
     @OnError
     public void onError(Session session, Throwable t) {
-        System.out.println("Error: " + t.getMessage());
+        log.warning("Error: " + t.getMessage());
     }
 }
