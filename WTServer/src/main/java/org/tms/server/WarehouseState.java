25270541982c79c6b86468cfa901d0494806350e
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-03-15 21:34:22 -0400

    Implement WarehouseState DTO

--- WTServer/src/main/java/org/tms/server/WarehouseState.java
+++ WTServer/src/main/java/org/tms/server/WarehouseState.java
@@ -1,4 +1,22 @@
 package org.tms.server;
 
+    import java.util.List;
+
 public class WarehouseState {
+    private final List<TruckDriver> waitingTruckDrivers;
+
+    private final List<TruckDriver> dockingTruckDrivers;
+
+    public WarehouseState(List<TruckDriver> waitingTruckDrivers, List<TruckDriver> dockingTruckDrivers) {
+        this.waitingTruckDrivers = waitingTruckDrivers;
+        this.dockingTruckDrivers = dockingTruckDrivers;
+    }
+
+    public List<TruckDriver> getWaitingTruckDrivers() {
+        return waitingTruckDrivers;
+    }
+
+    public List<TruckDriver> getDockingTruckDrivers() {
+        return dockingTruckDrivers;
+    }
 }
