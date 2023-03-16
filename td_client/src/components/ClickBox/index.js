586d75f02a1eae18c1578924e17eeb0efbb1a397
NOR0i <noroichan666@gmail.com>
2023-03-15 21:46:17 -0400

    Implemented Check-In menu

---
+++ td_client/src/components/ClickBox/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+function ClickBox({text}) {
+    return (
+      <input 
+        type="submit" 
+        className="ClickBox-Input" 
+        value={text}
+      />
+    );
+  }
+  
+  export default ClickBox;
