4568665e7c64042b55f39878d057a116d84d0a7f
QRiche <18075475+QRiche@users.noreply.github.com>
2023-03-19 22:43:59 -0400

    NotificationService: add method stubs

--- WTServer/src/main/java/org/tms/server/NotificationService.java
+++ WTServer/src/main/java/org/tms/server/NotificationService.java
@@ -1,4 +1,24 @@
 package org.tms.server;
 
+import org.tms.server.websocket.TruckMessage;
+
 public class NotificationService {
+    /**
+     * NotificationService represents a service used to send notifications to the truck driver.
+     */
+
+    public NotificationService() {
+    }
+
+    public TruckState notifyTruckUpdatedState(TruckDriver driver, TruckState state) {
+        TruckMessage message = new TruckMessage(driver.getTruckID(), driver.getDriverName(), driver.getEstimatedDockingTime().toString());
+        // TODO: interact with the websocket to send the message
+        return state;
+    }
+
+    public void notifyTruckQueueCancelled(TruckDriver driver) {
+        TruckMessage message = new TruckMessage(driver.getTruckID(), driver.getDriverName(), null);
+        // TODO: interact with the websocket to send the message
+    }
+
 }
