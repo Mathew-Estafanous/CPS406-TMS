9e87d5475e37897049db4761f134e290bdddfad2
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-03-19 21:39:59 -0400

    [websockets] Session map to keep track of trucks and their open WS connections.

--- WTServer/src/main/java/org/tms/server/websocket/TruckServerController.java
+++ WTServer/src/main/java/org/tms/server/websocket/TruckServerController.java
@@ -5,28 +5,35 @@ import org.tms.server.TruckDriver;
 import org.tms.server.TruckState;
 
 import javax.websocket.*;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 import static org.tms.server.websocket.TruckMessage.MessageType.*;
 
-@ServerEndpoint(value = "/truck/server",
+@ServerEndpoint(value = "/server/{truckID}",
                 decoders = TruckMessage.TruckMessageDecoder.class,
                 encoders = TruckMessage.TruckMessageEncoder.class)
 public class TruckServerController {
 
     private static final Logger log = Logger.getLogger(TruckServerController.class.getName());
     private final ITruckService truckService;
+    private final ConcurrentHashMap<Integer, Session> sessionMap;
 
-    public TruckServerController(ITruckService truckService) {
+    public TruckServerController(ITruckService truckService, ConcurrentHashMap<Integer, Session> sessionMap) {
         this.truckService = truckService;
+        this.sessionMap = sessionMap;
     }
 
     @OnOpen
-    public void onOpen(Session session) throws IOException {
-        log.info("New connection opened");
+    public void onOpen(Session session,
+                       @PathParam("truckID") String truckID) {
+        log.info("New connection opened with ID: " + truckID);
+        sessionMap.put(Integer.getInteger(truckID), session);
     }
 
     @OnMessage
@@ -34,8 +41,8 @@ public class TruckServerController {
         log.info("Message received: " + message);
         switch (message.getType()) {
             case CHECK_IN -> handleCheckIn(session, message);
-            case CHECK_OUT -> handleCheckOut(message);
-            case STATE_UPDATE -> handleStateUpdate(message);
+            case CHECK_OUT -> handleCheckOut(session, message);
+            case STATE_UPDATE -> handleStateUpdate(session, message);
         }
     }
 
@@ -47,21 +54,39 @@ public class TruckServerController {
         try {
             session.getBasicRemote().sendObject(response);
         } catch (EncodeException e) {
-            log.warning(String.format("Failed to encode check-in response object: %s", e.getMessage()));
+            log.warning("Failed to encode check-in response object: " + e.getMessage());
         }
     }
 
-    private void handleCheckOut(TruckMessage message) {
-        truckService.checkOut(message.getTruckID());
+    private void handleCheckOut(Session session, TruckMessage message) throws IOException {
+        final TruckState state = truckService.checkOut(message.getTruckID());
+        final TruckMessage response = new TruckMessage(state.getTruckID(), CHECK_OUT, false, 0);
+        try {
+            session.getBasicRemote().sendObject(response);
+        } catch (EncodeException e) {
+            log.warning("Failed to encode check-in response object: " + e.getMessage());
+        }
     }
 
-    private void handleStateUpdate(TruckMessage message) {
-        truckService.getEntireTruckState(message.getTruckID());
+    private void handleStateUpdate(Session session, TruckMessage message) throws IOException {
+        final TruckState state = truckService.getEntireTruckState(message.getTruckID());
+        final TruckMessage response = new TruckMessage(state.getTruckID(), STATE_UPDATE, state.getInWaitingArea(), state.getPosition());
+        try {
+            session.getBasicRemote().sendObject(response);
+        } catch (EncodeException e) {
+            log.warning("Failed to encode check-in response object: " + e.getMessage());
+        }
     }
 
     @OnClose
-    public void onClose(Session session) throws IOException {
-        log.info("Connection closed");
+    public void onClose(Session session) {
+        final var sessionEntry = sessionMap.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(session))
+                .findFirst();
+        if (sessionEntry.isEmpty()) return;
+        final Map.Entry<Integer, Session> closedSession = sessionEntry.get();
+        sessionMap.remove(closedSession.getKey());
+        log.info("Connection closed for session: " + closedSession.getKey());
     }
 
     @OnError
--- WTServer/src/main/java/org/tms/server/websocket/TruckWebsocketConfigurator.java
+++ WTServer/src/main/java/org/tms/server/websocket/TruckWebsocketConfigurator.java
@@ -2,20 +2,24 @@ package org.tms.server.websocket;
 
 import org.tms.server.ITruckService;
 
+import javax.websocket.Session;
 import javax.websocket.server.ServerEndpointConfig;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TruckWebsocketConfigurator extends ServerEndpointConfig.Configurator {
 
     private final ITruckService truckService;
+    private final ConcurrentHashMap<Integer, Session> sessionMap;
 
-    public TruckWebsocketConfigurator(ITruckService truckService) {
+    public TruckWebsocketConfigurator(ITruckService truckService, ConcurrentHashMap<Integer, Session> sessionMap) {
         this.truckService = truckService;
+        this.sessionMap = sessionMap;
     }
 
     @Override
     public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
         if (endpointClass == TruckServerController.class) {
-            return (T) new TruckServerController(truckService);
+            return (T) new TruckServerController(truckService, sessionMap);
         }
         return super.getEndpointInstance(endpointClass);
     }
