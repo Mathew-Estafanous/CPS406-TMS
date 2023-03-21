1398f14d713aaf8d7fcad10a9892fc05eceadc31
Mathew-Estafanous <mathewestafanous13@gmail.com>
2023-03-21 19:49:36 -0400

    Change to `Map` instead of explicit declaration of `ConcurrentHashMap`

--- WTServer/src/main/java/org/tms/server/Main.java
+++ WTServer/src/main/java/org/tms/server/Main.java
@@ -3,12 +3,13 @@ package org.tms.server;
 import org.tms.server.websocket.TruckWebsocketServer;
 
 import javax.websocket.Session;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
         WarehouseServer warehouseServer = new WarehouseServer(new TruckWaitingQueue(), new DockingAreaManager(), new NotificationService());
-        final ConcurrentHashMap<Integer, Session> sessionMap = new ConcurrentHashMap<>();
+        final Map<Integer, Session> sessionMap = new ConcurrentHashMap<>();
         final TruckWebsocketServer truckWsServer = new TruckWebsocketServer(8080, warehouseServer, sessionMap);
         try {
             truckWsServer.start();
