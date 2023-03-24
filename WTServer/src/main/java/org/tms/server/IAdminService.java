505dc132829adc15bb5d46278325631ee8fa7a25
RishieNagarajah <57832213+RishieNagarajah@users.noreply.github.com>
2023-03-24 13:52:05 -0400

    redone canceltruck

--- WTServer/src/main/java/org/tms/server/IAdminService.java
+++ WTServer/src/main/java/org/tms/server/IAdminService.java
@@ -3,6 +3,16 @@ package org.tms.server;
 public interface IAdminService {
 
     /**
+     * Cancels a truck that is either in the waiting area or docking area.
+     * @param truckId the id of the truck to cancel
+     * @return the driver of the truck that was cancelled
+     * @throws IllegalArgumentException if the truck is not in the waiting area or docking area
+     */
+    TruckDriver cancelTruck(int truckId) throws IllegalArgumentException;
+
+
+
+    /**
      * Changes the position of a truck in the waiting area.
      * @param truckId the id of the truck to change the position of
      * @param newPosition the new position of the truck (0 is the first position)
