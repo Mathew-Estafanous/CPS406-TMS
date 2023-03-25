f3e066ab488ff03779a78f19e736e501047702d2
NOR0i <noroichan666@gmail.com>
2023-03-25 14:29:00 -0400

    [front end] UI changes for Warehouse State.

---
+++ td_client/src/components/List/List.js
@@ -0,0 +1,21 @@
+import "../WarehouseStateMenu/WarehouseStateMenu.css"
+function List({list, title}) {
+    return (
+        <div className={"WarehouseStateMenu-listContainer"}>
+            <div>
+                <div className={"WarehouseStateMenu-header"}>{title}</div>
+                    {list.map((item, _) => {
+                        return (
+                            <div className={"WarehouseStateMenu-listItem"}>
+                                <div className={"textLeft"}>{item.driverName} </div>
+                                <div className={"textRight"}>Truck ID: {item.id}</div>
+                            </div>
+                        );
+                    })}
+                .
+            </div>
+        </div>
+    );
+}
+
+export default List;
--- td_client/src/components/WarehouseStateMenu/WarehouseStateMenu.js
+++ td_client/src/components/WarehouseStateMenu/WarehouseStateMenu.js
@@ -1,6 +1,8 @@
 import "./WarehouseStateMenu.css"
 import "../WhiteMenu/WhiteMenu.css"
 import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import DraggableList from "../DraggableList/DraggableList";
+import List from "../List/List";
 
 const warehouseState = {
     warehouseStateList: {
@@ -42,70 +44,19 @@ const warehouseState = {
         localStorage.setItem("warehouseState", JSON.stringify(list));
     }
 }
-
+//    localStorage.setItem("warehouseState", JSON.stringify(warehouseState.warehouseStateList))
 
 function WarehouseStateMenu() {
     const list = warehouseState.getWarehouseState();
     const waitingAreaList = list.waitingAreaList;
     const dockingAreaList = list.dockingAreaList;
+    console.log(warehouseState.getWarehouseState())
     return (
         <div className={"WarehouseStateMenu"}>
             <div className={"WarehouseStateMenu-title"}>Warehouse State</div>
             <div className={"Divider"}></div>
-            <div className={"WarehouseStateMenu-listContainer"}>
-                <DragDropContext onDragEnd={(dragItem) => {
-                    const sourceIndex = dragItem.source.index;
-                    const destIndex = dragItem.destination.index;
-                    dockingAreaList.splice(destIndex, 0, dockingAreaList.splice(sourceIndex, 1)[0]);
-                }}>
-                    <Droppable droppableId={"droppable-1"}>
-                        {(provided, _) => (
-                            <div ref={provided.innerRef} {...provided.droppableProps}>
-                                <div className={"WarehouseStateMenu-header"}>Docking Area</div>
-                                {dockingAreaList.map((item, i) => (
-                                    <Draggable key={item.id} draggableId={"draggable-" + item.id} index={i}>
-                                        {(provided, _) => (
-                                            <div className={"WarehouseStateMenu-listItem"}
-                                                 ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                {item.driverName}
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                                .
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
-            </div>
-            <div className={"WarehouseStateMenu-listContainer"}>
-                <DragDropContext onDragEnd={(dragItem) => {
-                    const sourceIndex = dragItem.source.index;
-                    const destIndex = dragItem.destination.index;
-                    waitingAreaList.splice(destIndex, 0, waitingAreaList.splice(sourceIndex, 1)[0]);
-                }}>
-                    <Droppable droppableId={"droppable-2"}>
-                        {(provided, _) => (
-                            <div ref={provided.innerRef} {...provided.droppableProps}>
-                                <div className={"WarehouseStateMenu-header"}>Waiting Area</div>
-                                {waitingAreaList.map((item, i) => (
-                                    <Draggable key={item.id} draggableId={"draggable-" + item.id} index={i}>
-                                        {(provided, _) => (
-                                            <div className={"WarehouseStateMenu-listItem"}
-                                                 ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                {item.driverName}
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                                {provided.placeholder}
-                                .
-                            </div>
-                        )}
-                    </Droppable>
-                </DragDropContext>
-            </div>
+            <List list={dockingAreaList} title={"Docking Area"}/>
+            <DraggableList list={waitingAreaList} title={"Waiting Area"} warehouseState={list}/>
         </div>
     );
 }
